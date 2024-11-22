d5d7a55f064cb5015fef6dfca82a6395c9ec14a4
mashal7 <mashal7@mail.ru>
2024-11-22 12:34:10 +0300

    example.py
    example for practic

--- example.py
+++ example.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
-import math
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('detach', True)
